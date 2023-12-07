cb7e10f1c2ab4f8b3480f5f5d9358302c205740c
Ruka1101 <hikaru.9286@gmail.com>
2023-12-07 13:46:53 +0900

    ready for deploy on render 2

--- db/migrate/20231112153622_add_details_to_post.rb
+++ db/migrate/20231112153622_add_details_to_post.rb
@@ -3,5 +3,7 @@ class AddDetailsToPost < ActiveRecord::Migration[7.1]
     add_column :posts, :kind, :string
     add_column :posts, :date_and_time, :string
     add_column :posts, :place, :string
+    add_column :posts, :latitude, :float
+    add_column :posts, :longitude, :float
   end
 end
