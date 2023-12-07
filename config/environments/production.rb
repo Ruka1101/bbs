7889aad9f2fcb19ec98e47ff7aec3edcf3e865a5
Ruka1101 <hikaru.9286@gmail.com>
2023-12-07 17:42:52 +0900

    add smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,19 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'meow-bbs-for-finding-lost-cats.onrender.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:             'smtp.gmail.com',
+    port:                 587,
+    domein:              'gmail.com',
+    user_name:           ENV['SMTP_USERNAME'],
+    password:            ENV['SMTP_PASSWORD'],
+    authentication:      'plain',
+    enable_starttls_auto: true
+  }
 
   # Code is not reloaded between requests.
   config.enable_reloading = false
